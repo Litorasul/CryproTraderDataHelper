a9ef3f88e0bee2ea477e58eb67d8905fda86d877
Nikolay Kelov <n_kelov@yahoo.com>
2023-10-06 20:11:36 +0300

    Add SimpleMovingAverageCalculator in SimpleMovingAverageBusinessLogicService

--- CryptoTraderDataHelperAPI/CryptoTraderDataHelperAPI/Services/BusinessLogic/SimpleMovingAverageBusinessLogicService.cs
+++ CryptoTraderDataHelperAPI/CryptoTraderDataHelperAPI/Services/BusinessLogic/SimpleMovingAverageBusinessLogicService.cs
@@ -4,6 +4,10 @@ namespace CryptoTraderDataHelperAPI.Services.BusinessLogic;
 
 public class SimpleMovingAverageBusinessLogicService : ISimpleMovingAverageBusinessLogicService
 {
+    public SimpleMovingAverageBusinessLogicService()
+    {
+    }
+
     public SimpleMovingAverageDto CalculateSimpleMovingAverage(int symbolId, int dataPoints, int timeAmount, TimePeriod timePeriod)
     {
         return CalculateSimpleMovingAverage(symbolId, dataPoints, timeAmount, timePeriod, DateTime.Now);
@@ -12,5 +16,21 @@ public class SimpleMovingAverageBusinessLogicService : ISimpleMovingAverageBusin
     public SimpleMovingAverageDto CalculateSimpleMovingAverage(int symbolId, int dataPoints, int timeAmount, TimePeriod timePeriod, DateTime startDate)
     {
         return new SimpleMovingAverageDto();
+    }
+
+    private List<double> SimpleMovingAverageCalculator(List<double> source, int length)
+    {
+        var sample = new Queue<double>(length);
+        var result = new List<double>();
+        foreach (var d in source)
+        {
+            if (sample.Count == length)
+            {
+                sample.Dequeue();
+            }
+            sample.Enqueue(d);
+            result.Add(sample.Average());
+        }
+        return result;
     }
 }
